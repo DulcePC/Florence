2dad5bf4b9b03da0595a5e8e5f183699e25c4d1d
Dulce <dulcefrancelispc@gmail.com>
2018-04-07 19:32:56 -0400

    section contact change

--- js/main.js
+++ js/main.js
@@ -1,26 +1,26 @@
 $(document).ready(function () {
-    $('.parallax').parallax();
-
-   // setInterval(function(){
-      // $('.carousel').carousel('next');
-    //},3000);
-
+    $('.fixed-action-btn').floatingActionButton();
+    $('.carousel.carousel-slider').carousel({
+        fullWidth:true,
+        indicators:false,
+      });
+    // setInterval(function(){
+    //  $('.carousel').carousel('next');
+    // },3000);
+   
     //slideshow
     $('a[href^="#"]').on('click', function (e) {
         e.preventDefault();
 
         var target = this.hash;
         var $target = $(target);
-
         $('html, body').animate({
-            'scrollTop': $target.offset().top
+            'scrollTop': $target.offset().top,
+
         }, 1000, 'swing');
+        
     });
 
-    $('.carousel.carousel-slider').carousel({
-        fullWidth:true,
-        indicators:true,
-      });
 });
 
 
